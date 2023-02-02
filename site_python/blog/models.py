0d988fcdc6bda4472fa666500a208ce2247c49c3
messe157 <emersongravacoes@gmail.com>
2023-02-02 20:05:41 -0300

    criação de postagem e listagem no painel admin

---
+++ site_python/blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User #importando bilbiotcas
+
+# Create your models here.
+
+STATUS = (
+    (0, "Rascunho"),
+    (1, "Publicar"),
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE, related_name='blog_post')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.BigIntegerField(choices=STATUS, default=0)
+
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
